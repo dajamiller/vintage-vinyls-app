87a83272fcfef06ebe3001c144ea250780e50c5b
Daja Miller <dajamiller96@gmail.com>
2025-12-16 21:23:53 -0800

    updated banner added rest controller + other imports

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -11,7 +13,9 @@ import org.yearup.models.User;
 import java.security.Principal;
 
 // convert this class to a REST controller
+@RestController
 // only logged in users should have access to these actions
+@PreAuthorize("permitUsers()") // ??
 public class ShoppingCartController
 {
     // a shopping cart requires
