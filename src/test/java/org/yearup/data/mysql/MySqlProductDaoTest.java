2705b10d706a34911d1d1b43c1fce345231e5a33
Daja Miller <dajamiller96@gmail.com>
2025-12-18 11:16:39 -0800

    resolves #6

--- src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
+++ src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
@@ -10,7 +10,6 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MySqlProductDaoTest extends BaseDaoTestClass
 {
